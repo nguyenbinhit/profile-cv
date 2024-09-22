52f851d6e37c7a37f02645e0145590efaa50cfa0
Nguyen Binh <nguyenbinhltv@gmail.com>
2024-09-22 15:56:01 +0700

    download cv - update contact

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,21 @@
+let downloadInitiated = false;
+
 document
-  .getElementById("download-profile-cv")
-  .addEventListener("click", function () {
-    // Create an anchor element
-    var link = document.createElement("a");
-    link.href = "assets/cv-nguyenkhacbinh.pdf";
-    link.download = "cv-nguyenkhacbinh.pdf";
-    link.click();
+  .querySelectorAll(".download-profile-cv, .download-profile-cv span")
+  .forEach(function (element) {
+    element.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default anchor behavior
+      if (!downloadInitiated) {
+        downloadInitiated = true;
+        // Create an anchor element
+        var link = document.createElement("a");
+        link.href = "assets/cv-nguyenkhacbinh.pdf";
+        link.download = "cv-nguyenkhacbinh.pdf";
+        link.click();
+        // Reset the flag after a short delay to allow for subsequent downloads
+        setTimeout(() => {
+          downloadInitiated = false;
+        }, 1000);
+      }
+    });
   });
